eb69ea20d5757a4878849932643063fda3ddb32a
dio <tianmingliang@ucsb.edu>
2023-05-11 00:39:42 -0700

    resolve conflict

--- Cavendish/outcome.py
+++ Cavendish/outcome.py
@@ -11,20 +11,22 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def func(t, a, lamb, gamma, phi):
-    return a * np.exp(-lamb*t) * np.cos(gamma*t + phi)
+def func(t, a, lamb, gamma, phi, c):
+    return a * np.exp(-lamb*t) * np.cos(gamma*t + phi) + c
 
 
 def get_fit_para(x, y, y_uncer):
 
 
-    popt, pcov = curve_fit(func, x, y, p0=[2, 0.001, 0.01, np.pi/2.5], 
+    popt, pcov = curve_fit(func, x, y, p0=[2, 0.001, 0.01, np.pi/2.5, 51], 
                         sigma=y_uncer, absolute_sigma=True)
     
-    para_names = ["amplitude", "decay_const", "ang_freq", "phase"]
-    for i in range(4):
+    para_names = ["amplitude", "decay_const", "ang_freq", "phase", "const"]
+    for i in range(len(para_names)):
         print(f"{para_names[i]} = {popt[i]} +- {np.sqrt(pcov[i][i])}")
 
+    ang_freq = popt[2]
+    const = popt[4]
 
     fig, ax = plt.subplots(figsize=(10, 5))
     ax.errorbar(x, y, 
@@ -33,11 +35,22 @@ def get_fit_para(x, y, y_uncer):
                 markersize=1, fmt="o", 
                 capthick=1.5, capsize=2, 
                 label="Experiment Data")
-    pred = func(x, popt[0], popt[1], popt[2], popt[3])
+    pred = func(x, popt[0], popt[1], popt[2], popt[3], popt[4])
     ax.plot(x, pred, label="Prediction")
 
     ax.set_title("Prediction")
     ax.set_xlabel("Time / s")
     ax.set_ylabel("Displacement / m")
     ax.legend()
+
+    
+    return ang_freq, const
+
+
+def get_G(delta_S, T, b, d, r, m1, L):
+    factor = np.pi ** (2) * delta_S * b ** (2)
+    nominator = d**(2) + (2/5) * r ** 2
+    denominator = T**2 * m1 * L * d
+    return factor * nominator / denominator
+
 
--- Cavendish/pre_process.py
+++ Cavendish/pre_process.py
@@ -5,82 +5,125 @@
 #
 #
 
-
-
 from libtiff import TIFF
+import csv 
 import numpy as np
 import matplotlib.pyplot as plt
 
 
-def mouse_event(event):
-   print('x: {} and y: {}'.format(event.xdata, event.ydata))
-
-
-def get_images(file_location, plotting=True):
+def preview_images(file_location, image_number, plotting=True):
     tif = TIFF.open(file_location)
-    image = tif.read_image()
-    image_len = 0
-    for image in tif.iter_images():
-        image_len += 1
+    img_num = 0
 
-    if plotting==True:
-        fig = plt.figure()
-        cid = fig.canvas.mpl_connect('button_press_event', mouse_event)
-    
-        plt.imshow(image)
+    for image in tif.iter_images():
+        img = image
+        img_num += 1
+        if img_num >= image_number:
+            break
+    if plotting == True:
+        fig, ax = plt.subplots(figsize=(10, 10))
+        ax.imshow(image)
+        plt.yticks(np.arange(0, len(image), 20))
+        plt.xticks(np.arange(0, len(image[0]), 50))
         plt.show()
 
-
-    return image_len, tif
-
+    return img
 
 
-def gen_pos_list(tif, x_bound, y_bound, 
-                 init_pos_cm, pix_to_cm, rgb_laser):
-    
-    pos_list = [init_pos_cm]
-    num_img = 0
+def get_laser_pos(image):
+    y_len = np.shape(image)[0]
+    x_len = np.shape(image)[1]
+    x_sum, y_sum = [], []
+    for i in range(x_len):
+        for j in range(y_len):
+            if (abs(image[j][i][0] - 255) <= 2):
+                x_sum.append(i)
+                y_sum.append(j)
+            else:
+                pass
+        x_mean, y_mean = np.array(x_sum).mean(), np.array(y_sum).mean()
+    return y_mean, x_mean
+
+
+def cal_dist(pos1, pos2):
+    diff_y = pos1[0] - pos2[0]
+    diff_x = pos1[1] - pos2[1]
+    return np.sqrt(diff_y**2 + diff_x**2)
+
+
+def cal_conver_factor(x1, x2, pos1, pos2):
+    return abs(x1-x2)/cal_dist(pos1, pos2)
+
+
+def conver_factor_and_init_pos(y_bound):
+    folder = ["./s1/", "./s2/"]
+
+    conver_list = []
+    init_pos_list = []
+    for f in range(len(folder)):
+        conver_fac = []
+        with open(folder[f] + "conver.csv") as csvfile:
+            read_data = csv.reader(csvfile, delimiter=',')
+            for row in read_data:
+                conver_fac.append(row)
+
+        f_location = []
+        for i in range(len(conver_fac)):
+            f_location.append(folder[f] + f"pos{f+1}_{i+1}.tif")
+
+        init_pos = []
+        for i in range(len(conver_fac)):
+            x = []
+            pos = [[], []]
+            for j in range(2):
+                if j == 0:
+                    init_pos.append(float(conver_fac[i][2*j+1]))
+                image = preview_images(f_location[i], int(conver_fac[i][2*j]), plotting=False)
+                image = image[y_bound[0]:y_bound[1]]
+                pix_y, pix_x = get_laser_pos(image)
+                pos[j].append(pix_y)
+                pos[j].append(pix_x)
+                x.append(float(conver_fac[i][2*j+1]))
+            conv = cal_conver_factor(x[0], x[1], pos[0], pos[1])
+            conver_list.append(conv)
+        init_pos_list.append(init_pos)
+    conver_mean = np.array(conver_list).mean()
+    conver_std = np.array(conver_list).std()
+    print(f"\n\n\nThe average of the conversion is {conver_mean} with std of {conver_std}")
+    print("Conversion_list = ", conver_list, "\n", "initial_position_list = ", init_pos_list)
+    print("\n\n\n")
+
+    return conver_mean, conver_std, init_pos_list
+
+
+def gen_pix_pos(file_location, y_bound):
+    tif = TIFF.open(file_location)
+    pix_pos = []
+    img_num = 0
     for image in tif.iter_images():
-        pix_loc = []
-        pix_color = []
-        for j in range(y_bound[0], y_bound[1], 1):
-            for i in range(x_bound[0], x_bound[1], 1):
-                pix_loc.append([j, i])
-                pix_color.append(image[j][i][:])
-
-        pix_loc = np.array(pix_loc)
-        pix_color = np.array(pix_color)
-
-        loc_laser = 0
-        pix_laser = 0
-        compare = 2000
-        for i in range(len(pix_color)):
-            dist = np.sqrt(rgb_laser.dot(pix_color[i]))
-            if dist < compare:
-                compare = dist
-                pix_laser = pix_color[i]
-                loc_laser = pix_loc[i]
-
-        num_img += 1
-        if num_img == 1:
-            init_pos_pix = loc_laser
-        else:
-            pix_diff = init_pos_pix - loc_laser
-            pix_dist = np.sqrt(np.dot(pix_diff, pix_diff))
-            pos_list.append(init_pos_pix + pix_dist * pix_to_cm)
-            
-        print(f"The laser pixel with rgb_val {pix_laser} at location {loc_laser}")
-    return pos_list
+        image = image[y_bound[0]:y_bound[1]][:][:]
+        y_mean, x_mean = get_laser_pos(image)
+        pix_pos.append(x_mean)
+        img_num += 1
+        
+    return pix_pos, img_num
 
 
-def get_xy(image_len, pos_list, T):
+def get_x(pix_pos, x1, conver):
+    init_pix = pix_pos[0]
+    rel_dist = []
+    for i in range(len(pix_pos)):
+        rel_dist.append(init_pix - pix_pos[i])
+    pos_cm = np.array(rel_dist) * conver + x1
+    return pos_cm
+
+
+def plot_dots(image_len, pos_cm, T):
     x = np.arange(T, T * image_len+1, T)
-    y = pos_list
+    y = pos_cm
     plt.figure(figsize=(10, 5))
     plt.scatter(x, y)
     plt.title("Laser Points displacement")
     plt.xlabel("Time / s")
     plt.ylabel("Displacement / m")
     return x, y
-
-
